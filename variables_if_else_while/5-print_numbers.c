50714912ea766b4cdb224adcd717f159a19a9fad
aaron <aaronjau21@gmail.com>
2022-06-08 23:22:33 -0700

    print

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
+/**
+ * main - Entry point
+ * Description - 'Creating a random number and if it positive or negative'
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int number;
+	int number = 0;
 
 	while (number < 10)
 	{
