ce3ae0704d5fd96b606f82ff7c455fe9e83d0a31
aaron <aaronjau21@gmail.com>
2022-06-08 22:07:32 -0700

    putchar

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Description - 'Creating a random number and if it positive or negative'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar("\n");
+	return (0);
+}
