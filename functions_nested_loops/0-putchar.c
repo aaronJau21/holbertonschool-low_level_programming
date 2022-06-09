0beaad2b8f632ed57563b8e6cc808f65a8e5bffc
aaron <aaronjau21@gmail.com>
2022-06-09 16:58:23 -0700

    putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/*
+ * main - principal function
+ *
+ * Description - 'Creating a random number and if it positive or negative
+ *
+ * return - array 0
+ */
+int main(void)
+{
+	int array = "_putchar";
+
+	ptchar(array);
+
+	return (0);
+}
