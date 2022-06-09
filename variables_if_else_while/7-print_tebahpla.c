f7811c337746b17780068de13901ee74944dcef3
aaron <aaronjau21@gmail.com>
2022-06-08 23:56:13 -0700

    7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ *main - print alphabet reversed using putchar
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int l = 'z';
+
+	while (l >= 'a')
+	{
+		putchar('l');
+		l--;
+	}
+	putchar('\n');
+
+	return (0);
+}
