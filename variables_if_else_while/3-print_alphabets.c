b968eaf6454d40a1385eef64de0822e0c63048d1
aaron <aaronjau21@gmail.com>
2022-06-08 22:46:15 -0700

    alphabets

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description - 'Creating a random number and if it positive or negative'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int lowerCase = 'a';
+	int upperCase = 'A';
+
+	while (lowerCase <= 'z')
+	{
+		putchar(lowerCase);
+		lowerCase++;
+	}
+
+	while (upperCase <= 'Z')
+	{
+		putchar(upperCase);
+		ypperCase++
+	}
+	putchar('\n');
+
+	return (0);
+}
