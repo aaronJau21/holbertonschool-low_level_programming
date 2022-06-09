c4ff941d81c2842e142c5ab2d82cbfe33fe77403
aaron <aaronjau21@gmail.com>
2022-06-08 23:33:47 -0700

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description - 'Creating a random number and if it positive or negative'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		putchar(number);
+		number++;
+	}
+	putchat('\n');
+
+	return (0);
+}
