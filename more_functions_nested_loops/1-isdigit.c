3d4069ed38e844c0574528a66093b51c2e1db8b2
aaron <aaronjau21@gmail.com>
2022-06-14 13:21:56 -0700

    1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _isdigit - code
+  * Description : Evaluate if its a digit
+  * @c:The charaacter
+  * Return:1 if its digit, else 0
+  */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
