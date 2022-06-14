386613bdc7c0d2625f4844c9cd52a6d5ad7ce209
aaron <aaronjau21@gmail.com>
2022-06-14 16:40:13 -0700

    3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
-  * print_numbers - prints 0 to 9
-  */
+ * print_numbers - prints 0 to 9
+ */
 void print_numbers(void)
 {
-	int n = '0';
+	char n = '0';
 
-	while (n == '9')
+	while (n <= '9')
 	{
 		_putchar(n);
 		n++;
 	}
+
 	_putchar('\n');
 }
