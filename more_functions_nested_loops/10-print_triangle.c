23f7a07f68d05289f35687d5ccdf2cb25f99b196
aaron <aaronjau21@gmail.com>
2022-06-14 23:33:32 -0700

    10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - print 'triangule'
+ * @size: size of
+ */
+
+void print_triangle(int size)
+{
+	int tri, al, base;
+
+	if (size < 0)
+		_putchar('\n');
+
+	for (tri = 1; tri <= size; tri++)
+	{
+		for (al = 1; al <= (size - tri); al++)
+			_putchar(' ');
+
+		for (base = 1; base <= tri; base++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+}
