8e3e1bb381e89ba763b86ae8459e378076d1b483
aaron <aaronjau21@gmail.com>
2022-06-16 16:58:52 -0700

    4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+  * print_rev - Print in reverse
+  * Description: Show each char of the string
+  * @s: The char
+  */
+void print_rev(char *s)
+{
+	int al, re;
+	re = strlen(s);
+
+	for (al = re - 1; al >= 0; al--)
+	{
+		_putchar(s[al]);
+	}
+	_putchar('\n');
+}
