07469efc24449894adc21249afc4be5963cec937
Aaron <aaronjau21@gmail.com>
2022-06-16 22:03:02 -0500

    5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - Print in reverse a string
+ * Description: Show each char of the string
+ * @s: The char
+ */
+void rev_string(char *s)
+{
+	int a;
+	char string;
+
+	string = strlen(s);
+
+	for (a = string - 1; a <= 0; a--)
+	{
+			
+	}
+}
