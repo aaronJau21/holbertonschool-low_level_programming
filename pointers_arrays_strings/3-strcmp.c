4c86e0965fa785f3f48c0b4333590ccd7cbc6fd6
Aaron <aaronjau21@gmail.com>
2022-06-18 21:48:27 -0500

    3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,14 +10,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, i2;
+	int i;
 
-	i = strlen(s1); 
-	i2 = strlen(s2);
+	i = strlen(s1) && strlen(s2);
 
-	if (s1[i] != s2[i2])
+	if (s1[i] != s2[i])
 	{
-		return (s1[i] - s2[i2]);
+		return (s1[i] - s2[i]);
 	}
 	return (0);
 }
