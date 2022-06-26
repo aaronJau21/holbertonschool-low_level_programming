b49bcf3319e1be7ac52eb3b8cbb967a0e609a048
Aaron <aaronjau21@gmail.com>
2022-06-26 01:18:54 -0500

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -  writes nnumber of args
+ * @argc: counter
+ * @argv: value
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
