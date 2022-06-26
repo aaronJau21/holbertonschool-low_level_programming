4161773c33d454bd4feb144b8751f2eb92f7285f
Aaron <aaronjau21@gmail.com>
2022-06-26 01:17:55 -0500

    0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -  writes name
+ * @argc: counter
+ * @argv: value
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", *argv);
+	return (0);
+}
