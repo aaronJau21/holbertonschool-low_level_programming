2a0ef959e1af978a7293af0a5b6c8dcb627a3a46
Aaron <aaronjau21@gmail.com>
2022-06-26 22:21:50 -0500

    3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -22,6 +22,8 @@ int **alloc_grid(int width, int height)
 		arr[i] = malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
 		{
+			for (--i; i >= 0; i--)
+				free(arr[i]);
 			free(arr);
 			return (NULL);
 		}
