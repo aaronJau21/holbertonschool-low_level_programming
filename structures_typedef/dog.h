f594fa38231eae5b3bb8f9dbc9b1dbf5b9f10bbe
Aaron <aaronjau21@gmail.com>
2022-07-01 09:46:20 -0500

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef HEADER
+#define HEADER dog.h
+/**
+ * struct dog - New struct dog with name, age, owner
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char  *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+#endif
