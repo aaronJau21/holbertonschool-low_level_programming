d18f3199bf4155f5c280eea6a3a4340fb4ec1ce6
Aaron <aaronjau21@gmail.com>
2022-07-20 23:11:13 -0500

    1-print_binary.c

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints the binary representation of a number.
+ *
+ * @n: Number to representate on binary
+ *
+ * Return: Void
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n == 0 || n == 1)
+	{
+		_putchar(n + '0');
+		return;
+	}
+
+	print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
